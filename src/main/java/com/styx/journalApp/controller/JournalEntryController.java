965875e3a51a41e152c4cad2cf3e9754edb15623
Sidharth <sidharthjainj25@gmail.com>
2024-07-07 19:09:01 +0530

    initial commit

---
+++ src/main/java/com/styx/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,46 @@
+package com.styx.journalApp.controller;
+
+import com.styx.journalApp.JournalApplication;
+import com.styx.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @GetMapping("/id/{journalId}")
+    public JournalEntry getJournalById(@PathVariable Long journalId){
+        return journalEntries.get(journalId);
+    }
+
+    @PostMapping
+    public boolean createJournal(@RequestBody JournalEntry journalEntry){
+        journalEntries.put(journalEntry.getId(), journalEntry);
+        return true;
+    }
+
+    @PutMapping
+    public boolean updateJournal(@RequestBody JournalEntry journalEntry){
+        journalEntries.put(journalEntry.getId(), journalEntry);
+        return true;
+    }
+
+    @DeleteMapping("/id/{journalId}")
+    public boolean deleteJournal(@PathVariable Long journalId){
+        journalEntries.remove(journalId);
+        return true;
+    }
+
+}
