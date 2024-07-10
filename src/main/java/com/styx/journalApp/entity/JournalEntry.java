2bcd932ec0b8d1e0b72c2f6af3758b7a83129e35
Sidharth <sidharthjainj25@gmail.com>
2024-07-11 00:39:38 +0530

    integrated lombok to reduce boilerplate code

--- src/main/java/com/styx/journalApp/entity/JournalEntry.java
+++ src/main/java/com/styx/journalApp/entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package com.styx.journalApp.entity;
 
+import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -7,43 +8,16 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Document(collection = "journalEntry")
+@Data   // Combined annotation for @Setter, @Getter, @RequiredArgsConstructor, @ToString, @EqualsAndHashCode, and @Value
+//@NoArgsConstructor        constructor with no arguments
+//@AllArgsConstructor       constructor with all arguments
+//@ToString                 not entirely sure
+//@EqualsAndHashCode        not entirely sure
+//@Builder                  not entirely sure
 public class JournalEntry {
     @Id
     private ObjectId id;
     private String title;
     private String description;
     private LocalDateTime date;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
 }
