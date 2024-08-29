eff70fd0bf44576082b70ef26b98368794e4a0e8
Sidharth <sidharthjainj25@gmail.com>
2024-08-29 23:05:22 +0530

    added spring security config class

---
+++ src/main/java/com/styx/journalApp/config/SpringSecurity.java
@@ -0,0 +1,17 @@
+package com.styx.journalApp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        super.configure(httpSecurity);
+    }
+}
