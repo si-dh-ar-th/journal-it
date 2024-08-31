e28307b961e74af86b779f972623a67b84c880fc
Sidharth <sidharthjainj25@gmail.com>
2024-08-31 23:12:30 +0530

    added user details service implementation

--- src/main/java/com/styx/journalApp/config/SpringSecurity.java
+++ src/main/java/com/styx/journalApp/config/SpringSecurity.java
@@ -2,7 +2,6 @@ package com.styx.journalApp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,6 +11,10 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
-        super.configure(httpSecurity);
+        httpSecurity.authorizeRequests()
+                .antMatchers("/journal/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .httpBasic();
     }
 }
