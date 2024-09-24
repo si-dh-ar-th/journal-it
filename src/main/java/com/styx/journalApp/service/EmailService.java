d735845da1e1744e000967429845ab65ea80ee90
Sidharth Jain <sidharthjainj25@gmail.com>
2024-09-24 23:46:15 +0530

    email functionality initialization

---
+++ src/main/java/com/styx/journalApp/service/EmailService.java
@@ -0,0 +1,8 @@
+package com.styx.journalApp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+}
---
+++ src/test/java/com/styx/journalApp/service/EmailServiceTests.java
@@ -0,0 +1,8 @@
+package com.styx.journalApp.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTests {
+
+}
