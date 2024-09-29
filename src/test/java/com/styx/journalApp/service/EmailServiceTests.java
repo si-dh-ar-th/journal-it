582ffa1075827208439be572c9045fa1f8bd9cb9
Sidharth Jain <sidharthjainj25@gmail.com>
2024-09-29 11:42:40 +0530

    added mock email service

--- src/test/java/com/styx/journalApp/service/EmailServiceTests.java
+++ src/test/java/com/styx/journalApp/service/EmailServiceTests.java
@@ -1,8 +1,20 @@
 package com.styx.journalApp.service;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 public class EmailServiceTests {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    void sendEmailTest(){
+        emailService.sendEmail("jetob89472@abevw.com", "Spring Boot Test", "Hi aap kaise hai?");
+    }
 
 }
