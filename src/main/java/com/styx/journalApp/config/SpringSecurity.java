90e06ee56bb0d72a568e768fd20b2af39b3e19bb
Sidharth Jain <sidharthjainj25@gmail.com>
2024-10-17 23:08:17 +0530

    added surrounding business logic to jwt tokens

--- src/main/java/com/styx/journalApp/config/SpringSecurity.java
+++ src/main/java/com/styx/journalApp/config/SpringSecurity.java
@@ -4,11 +4,14 @@ import com.styx.journalApp.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -36,8 +39,14 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    protected PasswordEncoder passwordEncoder(){
+    protected PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
 }
---
+++ src/main/java/com/styx/journalApp/utils/JwtUtils.java
@@ -0,0 +1,8 @@
+package com.styx.journalApp.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtils {
+
+}
